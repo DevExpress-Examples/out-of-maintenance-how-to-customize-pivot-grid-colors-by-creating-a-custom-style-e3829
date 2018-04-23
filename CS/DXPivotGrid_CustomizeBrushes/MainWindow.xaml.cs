963e1093c5b0d89d5f41633001779f43c834c453
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_CustomizeBrushes/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using DXPivotGrid_CustomizeBrushes.DataSet1TableAdapters;
+
+namespace DXPivotGrid_CustomizeBrushes {
+    public partial class MainWindow : Window {
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataAdapter.GetData();
+        }
+    }
+}
